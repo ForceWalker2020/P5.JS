2d0eb18d203ff796090d4b746f9bedebbb6d683d
ForceWalker2020 <84183620+ForceWalker2020@users.noreply.github.com>
2022-02-18 19:08:27 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function preload()
 
 function setup()
 {
-   canvas = createCanvas(800, 400);
+   canvas = createCanvas(800, 800);
    canvas.position(300, 300);
    video = createCapture(VIDEO);
    video.hide();
@@ -15,7 +15,19 @@ function setup()
 
 function draw()
 {
-   image(video, 0, 0, 640, 480);
+   image(video, 230, 150, 200, 240);
+   fill(255, 0, 0);
+   stroke(255, 0, 0);
+   circle(50, 50, 80);
+   circle(50, 430, 80);
+   circle(590, 50, 80);
+   circle(590, 430, 80);
+   fill(0, 128, 0);
+   stroke(0, 128, 0);
+   rect(90, 40, 460, 20);
+   rect(90, 420, 460, 20);
+   rect(40, 90, 20, 300);
+   rect(580, 90, 20, 300);
    tint(tint_color);
 }
 
